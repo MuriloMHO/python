67cb07248a624fb5331ea780bc5ae2f8c015edd6
Murilo Machado Hoffman de Oliveira <165489051+MuriloMHO@users.noreply.github.com>
2025-03-04 20:07:08 +0000

    More features added to the game

--- aliens_invasion/settings.py
+++ aliens_invasion/settings.py
@@ -4,6 +4,9 @@ class Settings:
     def __init__(self):
         """initialize the game's settings"""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 680
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 3
